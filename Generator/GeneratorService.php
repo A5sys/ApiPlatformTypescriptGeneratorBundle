5fb1b0791dc0cd8356d82214ae3e29b12155b5a4
Thomas Beaujean <t.beaujean@a5sys.com>
2018-12-07 16:50:44 +0100

    update

--- Generator/GeneratorService.php
+++ Generator/GeneratorService.php
@@ -4,6 +4,7 @@ namespace A5sys\ApiPlatformTypescriptGeneratorBundle\Generator;
 
 use ApiPlatform\Core\Metadata\Property\Factory\AnnotationPropertyMetadataFactory;
 use ApiPlatform\Core\Util\ReflectionClassRecursiveIterator;
+use function GuzzleHttp\Promise\unwrap;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PropertyInfo\Type;
 use Symfony\Component\Serializer\Mapping\AttributeMetadataInterface;
@@ -28,9 +29,9 @@ class GeneratorService
         $this->propertyMetadataFactory = $propertyMetadataFactory;
     }
 
-    public function generate(string $rootFolder): Response
+    public function generate(string $rootFolder): void
     {
-        $groupData = [];
+        $entities = [];
         foreach (ReflectionClassRecursiveIterator::getReflectionClassesFromDirectories($this->apiPlatformPaths) as $metas) {
             $className = $metas->getName();
             $metadata = $this->classMetadataFactory->getMetadataFor($className);
@@ -41,24 +42,22 @@ class GeneratorService
             foreach ($metas as $attributeName => $meta) {
                 $propertyMetadata = $this->propertyMetadataFactory->create($className, $attributeName);
 
-                $groups = $meta->getGroups();
-                foreach ($groups as $group) {
-                    $groupData[$group][$className][$attributeName] = [
-                        'type' => $propertyMetadata->getType(),
-                    ];
-                }
+                $entities[$className][$attributeName] = [
+                    'type' => $propertyMetadata->getType(),
+                ];
             }
         }
 
-        foreach ($groupData as $group => $data) {
-            foreach ($data as $className => $attributes) {
-                $reflection = new \ReflectionClass($className);
-                $parent = $reflection->getParentClass();
-                if ($parent) {
-                    $parentClassName = $parent->getName();
-                    if (key_exists($parentClassName, $groupData[$group])) {
-                        foreach ($attributes as $attributeName => $attribute) {
-                            $groupData[$group][$parentClassName][$attributeName] = $attribute;
+        foreach ($entities as $className => $attributes) {
+            $reflection = new \ReflectionClass($className);
+            $parent = $reflection->getParentClass();
+            if ($parent) {
+                $parentClassName = $parent->getName();
+                if (key_exists($parentClassName, $entities)) {
+                    foreach ($attributes as $attributeName => $attribute) {
+                        // do not overwrite parent values
+                        if (!key_exists($attributeName, $entities[$parentClassName])) {
+                            $entities[$parentClassName][$attributeName] = $attribute;
                         }
                     }
                 }
@@ -69,28 +68,38 @@ class GeneratorService
             mkdir($rootFolder);
         }
 
-        foreach ($groupData as $groupName => $entities) {
-            $groupFolder = $rootFolder.'/ws_'.$groupName;
+        foreach ($entities as $className => $attributes) {
+            $subfolder = $this->getFolderByClassname($className);
+            $filepath = $rootFolder.'/'.$this->getFileByClassname($className).'.ts';
 
-            if (!\file_exists($groupFolder)) {
-                mkdir($groupFolder);
+            $entityFolder = $rootFolder.'/'.$subfolder;
+            if (!\file_exists($entityFolder)) {
+                mkdir($entityFolder, 755, true);
             }
 
-            foreach ($entities as $entityName => $attributes) {
-                $filepath = $groupFolder.'/'.$this->cleanName($entityName).'.ts';
-                $content = $this->getTsContent($entityName, $attributes);
-                file_put_contents($filepath, $content);
-            }
+            $content = $this->getTsContent($className, $attributes);
+            file_put_contents($filepath, $content);
         }
+    }
 
-        return new Response();
+    private function fromCamelCase($input) {
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('_', $ret);
     }
 
     private function getTsContent(string $entityName, array $attributes): string
     {
         $attributesImportStrings = [];
-        foreach ($attributes as $attribute) {
-            $import = $this->getImportContent($attribute['type']);
+        foreach ($attributes as $name => $attribute) {
+            $type = $attribute['type'];
+            if ($type === null) {
+                throw new \LogicException('The type is null for '.$name.' element of the entity:'.$entityName);
+            }
+            $import = $this->getImportContent($type, $entityName);
             if ($import) {
                 $attributesImportStrings[] = $import;
             }
@@ -105,7 +114,7 @@ class GeneratorService
 
         return
 $attributesImport.'
-export class '.$this->cleanName($entityName).' {
+export class '.$this->getFileNameByClassname($entityName).' {
 '.$attributesContent.'}
 ';
     }
@@ -118,18 +127,18 @@ export class '.$this->cleanName($entityName).' {
         $strType = 'string';
 
         if ($entity) {
-            $strType = $this->getLocalPath($entity->getClassName()).'[]';
+            $strType = ': '.$this->getFileNameByClassname($entity->getClassName()).'[]';
         } else if ($entityClass) {
-            $strType = $this->getLocalPath($entityClass);
+            $strType = ': '.$this->getFileNameByClassname($entityClass);
         } else if ($builtinType) {
             $strType = $this->convertBuiltinTypeToTypescript($builtinType);
         }
 
-        if ($strType === 'DateTime') {
-            $strType = 'string';
+        if ($strType === ': DateTime') {
+            $strType = ': string';
         }
 
-        return  '  public '.$attributeName.': '.$strType.';'."\n";
+        return  '  public '.$attributeName.' '.$strType.';'."\n";
     }
 
     private function convertBuiltinTypeToTypescript(string $type): string
@@ -137,32 +146,43 @@ export class '.$this->cleanName($entityName).' {
         switch ($type) {
             case 'int':
             case 'float':
-                return 'number';
+                return ': number';
                 break;
             case 'bool':
-                return 'boolean';
+                return ': boolean';
+                break;
+            case 'array':
+                return '= []';
                 break;
             default:
-                return $type;
+                return ': '.$type;
         }
     }
 
-    private function getImportContent(Type $type) : ?string
+    private function getImportContent(Type $type, string $classname) : ?string
     {
         $entity = $type->getCollectionValueType();
         $entityClass = $type->getClassName();
         $strType = null;
         if ($entity) {
-            $strType = $this->getLocalPath($entity->getClassName());
+            if ($classname === $entity->getClassName()) {
+                return null;
+            }
+            $strType = $this->getFileNameByClassname($entity->getClassName());
+            $strTypePath = $this->getFileByClassname($entity->getClassName(), $classname);
         } else if ($entityClass) {
-            $strType = $this->getLocalPath($entityClass);
+            if ($classname === $entityClass) {
+                return null;
+            }
+            $strType = $this->getFileNameByClassname($entityClass);
+            $strTypePath = $this->getFileByClassname($entityClass, $classname);
         }
 
         if (!$strType || $strType === 'DateTime') {
             return null;
         }
 
-        return 'import { '.$strType.' } from \'./'.$strType.'\';'."\n";
+        return 'import { '.$strType.' } from \'./'.$strTypePath.'\';'."\n";
     }
 
 
@@ -179,5 +199,38 @@ export class '.$this->cleanName($entityName).' {
         }
 
         return $cleanName;
+    }
+
+    private function getFolderByClassname(string $className, $popLast = true): string
+    {
+        $splitted = explode('\\', $className);
+        $splittedKekabCase = [];
+        foreach ($splitted as $split) {
+            $splittedKekabCase[] = $this->fromCamelCase($split);
+        }
+        if ($popLast) {
+            // remove last element
+            array_pop($splittedKekabCase);
+        }
+
+        return join('/', $splittedKekabCase);
+    }
+
+    private function getFileByClassname(string $className, ?string $entityName = null): string
+    {
+        $previousPath = './';
+        if ($entityName) {
+            $splitted = explode('\\', $entityName);
+            $countPrevious = count($splitted) - 1;
+            $previousPath .= str_repeat('../', $countPrevious);
+        }
+        return $previousPath.$this->getFolderByClassname($className, false);
+    }
+
+    private function getFileNameByClassname(string $className): string
+    {
+        $splitted = explode('\\', $className);
+
+        return array_pop($splitted);
     }
 }
